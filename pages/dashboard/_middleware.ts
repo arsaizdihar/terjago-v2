3ef49e3236d2e674af00c600ef7e86bba00a8a2b
arsaizdihar <arsadihar@gmail.como>
2022-01-31 09:21:20 +0700

    fix dashboard middleware

--- pages/dashboard/_middleware.ts
+++ pages/dashboard/_middleware.ts
@@ -1,7 +1,11 @@
 import { NextMiddleware, NextResponse } from "next/server";
 
 export const middleware: NextMiddleware = async (req, ev) => {
-  const session = req.cookies["next-auth.session-token"];
+  const cookieName =
+    process.env.NODE_ENV === "development"
+      ? "next-auth.session-token"
+      : "__Secure-next-auth.session-token";
+  const session = req.cookies[cookieName];
   const url = req.nextUrl.clone();
   url.pathname = "/login";
   if (!session) {
